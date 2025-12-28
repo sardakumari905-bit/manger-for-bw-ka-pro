4eaa0e550f5db52b893a83e929f0753e387a334a
sardakumari905-bit <sardakumari905@gmail.com>
2025-12-28 23:17:51 +0530

    Add configuration settings in config.py
    Added configuration parameters for bot token, owner ID, database file, and startup image.

---
+++ config.py
@@ -0,0 +1,13 @@
+# --- CONFIGURATION ---
+
+# 1. Apna Bot Token Yahan Dalein
+BOT_TOKEN = "8265772489:AAESguanjhVh5Pb3OPrpkCz2YYTaOKH_hI8" 
+
+# 2. Apni Telegram ID (Number) Yahan Dalein
+OWNER_ID = 6761345074
+
+# Database File Name
+DB_FILE = "ultra_final_db.json"
+
+# Startup Photo Link
+START_IMG = "https://cdn-icons-png.flaticon.com/512/3408/3408591.png"
