6ab88f984601f4da1e3d162e2b965cb2b3b2daf4
sardakumari905-bit <sardakumari905@gmail.com>
2025-12-29 14:29:16 +0530

    Update bot token and database file name

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # --- CONFIGURATION ---
-BOT_TOKEN = "8125051462:AAFcL32CB_j0RDctz387OAXks2ZlsVpi0aE" 
-OWNER_ID = 6761345074  # Sirf Number
-DB_FILE = "scheduler_db.json"
+BOT_TOKEN = "8237289244:AAEXMVHy8hMyVKgtH5wA7md5_8WHIEzXXYE" 
+OWNER_ID = 6761345074 
+DB_FILE = "rbse_final_db.json"
 START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
