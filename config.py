78862d842eb8f03a972d5cccc40d6d3b5840638b
sardakumari905-bit <sardakumari905@gmail.com>
2025-12-29 14:03:43 +0530

    Update configuration values in config.py

--- config.py
+++ config.py
@@ -1,13 +1,5 @@
 # --- CONFIGURATION ---
-
-# 1. Apna Bot Token Yahan Dalein
-BOT_TOKEN = "8265772489:AAESguanjhVh5Pb3OPrpkCz2YYTaOKH_hI8" 
-
-# 2. Apni Telegram ID (Number) Yahan Dalein
-OWNER_ID = 6761345074
-
-# Database File Name
-DB_FILE = "ultra_final_db.json"
-
-# Startup Photo Link
-START_IMG = "https://cdn-icons-png.flaticon.com/512/3408/3408591.png"
+BOT_TOKEN = "8125051462:AAFcL32CB_j0RDctz387OAXks2ZlsVpi0aE" 
+OWNER_ID = 6761345074  # Sirf Number
+DB_FILE = "scheduler_db.json"
+START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
