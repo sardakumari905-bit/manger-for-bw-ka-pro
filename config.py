17e31a593f782908a6cd6a850ecc04135627b1ce
sardakumari905-bit <sardakumari905@gmail.com>
2025-12-30 12:40:22 +0530

    Change BOT_TOKEN in config.py
    Updated the BOT_TOKEN for the application.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # --- CONFIGURATION ---
-BOT_TOKEN = "8237289244:AAEXMVHy8hMyVKgtH5wA7md5_8WHIEzXXYE" 
+BOT_TOKEN = "7846479006:AAH-MnFhQLC2K8A0Zo-fZFG-OnHA0V_iATw" 
 OWNER_ID = 6761345074 
 DB_FILE = "rbse_final_db.json"
 START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
