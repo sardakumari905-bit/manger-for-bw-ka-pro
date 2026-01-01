8475d6fcb01afa0a60754e1658fd3b266a4b34c0
sardakumari905-bit <sardakumari905@gmail.com>
2026-01-01 19:44:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 BOT_TOKEN = "8548905075:AAEgZBvmGvwIbAHgH0LkW1q_IG7nIQTrh-Y" 
 OWNER_ID = 6761345074 
 DB_FILE = "rbse_final_db.json"
-START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
+START_IMG = "[url=https://ibb.co/fdgTFrhc][img]https://i.ibb.co/fdgTFrhc/Screenshot-2025-12-30-125804.png"
