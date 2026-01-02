150cc53d2b71b3814a93763afc83489846ffb2c3
sardakumari905-bit <sardakumari905@gmail.com>
2026-01-02 18:17:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,20 @@
-# config.py
-import os
+import pytz
+from datetime import date
 
-# ----------------- SETTINGS -----------------
-# Apna Token yahan daalein
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE" 
+# --- SETTINGS ---
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"  # Apna Token Dalein
+OWNER_ID = 1234567890              # Apni Admin ID Dalein
+MAIN_GROUP_ID = -100123456789      # Group ID
 
-# Apni Telegram ID (Admin) yahan daalein
-OWNER_ID = 1234567890 
-
-# Jis group me bot chalana hai, uski ID
-MAIN_GROUP_ID = -1001234567890 
-
-# Database file ka naam
 DB_FILE = "rbse_final_db.json"
-
-# Start image link
 START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
+
+# Global Constants
+IST = pytz.timezone('Asia/Kolkata')
+EXAM_DATE = date(2026, 2, 12)
+
+# Conversation States (Yahan declare kiye taaki sab use kar sakein)
+ASK_DATE, ASK_TOPIC, ASK_LINK, ASK_TIME_SLOT = range(4)
+ASK_BROADCAST_MSG = 4
+ASK_ADMIN_ID = 5
+ASK_TOPPER_NAME = 6
