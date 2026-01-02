c9c2f575ea78f332a0c266e324d064dfcfd9e94b
sardakumari905-bit <sardakumari905@gmail.com>
2026-01-02 18:13:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 # config.py
-# --- CONFIGURATION ---
-BOT_TOKEN = "8496826089:AAHOE-x3iMx6PJYcOb-Hgab82BkCski3zyo" 
-OWNER_ID = 6761345074 
-MAIN_GROUP_ID = -1003308402459  # <--- HARDCODED GROUP ID
+import os
+
+# ----------------- SETTINGS -----------------
+# Apna Token yahan daalein
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE" 
+
+# Apni Telegram ID (Admin) yahan daalein
+OWNER_ID = 1234567890 
+
+# Jis group me bot chalana hai, uski ID
+MAIN_GROUP_ID = -1001234567890 
+
+# Database file ka naam
 DB_FILE = "rbse_final_db.json"
+
+# Start image link
 START_IMG = "https://i.postimg.cc/rmDPsqRC/Gemini-Generated-Image-5jbjnc5jbjnc5jbj.png"
